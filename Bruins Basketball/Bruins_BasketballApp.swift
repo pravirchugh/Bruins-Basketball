8c6fcb09efac5526def086872e608003cce6564f
pravirchugh <pravirchugh@yahoo.com>
2023-11-29 00:44:49 -0800

    Reinitializing with SwiftUI

---
+++ Bruins Basketball/Bruins_BasketballApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bruins_BasketballApp.swift
+//  Bruins Basketball
+//
+//  Created by Pravir Chugh on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Bruins_BasketballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bruins BasketballUITests/Bruins_BasketballUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Bruins_BasketballUITestsLaunchTests.swift
+//  Bruins BasketballUITests
+//
+//  Created by Pravir Chugh on 11/29/23.
+//
+
+import XCTest
+
+final class Bruins_BasketballUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
